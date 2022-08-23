6b02575afb1e4cce05c06fe34a6bf774c6d55991
Camacio <sapadri@hotmail.com>
2022-08-23 12:46:32 +0200

    first commit

---
+++ calculator/src/components/container/Calculator.jsx
@@ -0,0 +1,11 @@
+import Result from "../pure/Result";
+
+const Calculator = () => {
+    return(
+        <div className="react-calculator">
+            <Result value={"0"}/>
+        </div>
+    )
+}
+
+export default Calculator;
---
+++ calculator/src/components/pure/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({type, text}) => {
+    return(
+        <div>
+            <span>{text}</span>
+        </div>
+    )
+}
+
+export default Button;
